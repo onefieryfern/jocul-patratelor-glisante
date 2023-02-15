2a64bc4644250ef939e144d4e4e7585ca1b8aec4
Andrei Florea <122226601+onefieryfern@users.noreply.github.com>
2023-02-15 07:27:21 +0000

    Clean up code
    Create variables for min and max board configs

--- main.cpp
+++ main.cpp
@@ -7,33 +7,41 @@ struct Point {
 };
 
 int main() {
+    // Window Properties
     constexpr short xAxisLength { 1280 };
     constexpr short yAxisLength { 720 };
 
     constexpr char title[] { "Jocul Patratelor Glisante" };
 
-    constexpr short numOfTilesOnSide { 5 };
-    const short tileSize { 80 };
+    // Title Properties
 
     const short verticalTitleSize { 100 };
     constexpr short titlePadding { 20 };
 
-    initwindow(xAxisLength, yAxisLength, title);
+    // Board Properties
+    constexpr short numOfTilesOnSide { 5 };
+
+    const short tileSize { 80 };
 
     const short boardSize { numOfTilesOnSide * tileSize };
     const short boardPadding { (yAxisLength - (titlePadding + verticalTitleSize) - boardSize) / 2 };
 
+    // Other setup
+
+    const short minBoardPadding { titlePadding };
+    const short maxBoardSize { yAxisLength - (titlePadding + verticalTitleSize + 2 * minBoardPadding) };
+    const short maxTileSize { static_cast<short>(maxBoardSize / numOfTilesOnSide) };
+
+    const short minTileSize { yAxisLength <= xAxisLength ? yAxisLength / 9 : xAxisLength / 16 };
+    const short minBoardSize { numOfTilesOnSide * minTileSize };
+    const short maxBoardPadding { (yAxisLength - (titlePadding + verticalTitleSize) - minBoardSize) / 2 };
+
+    initwindow(xAxisLength, yAxisLength, title);
+
     // Draw title placeholder
     rectangle(titlePadding, titlePadding, xAxisLength - titlePadding, verticalTitleSize + titlePadding);
 
-    const short spaceOccupiedByTitle { verticalTitleSize + 2 * titlePadding };
-
     Point boardTopLeft { (xAxisLength - boardSize) / 2, (titlePadding + verticalTitleSize + boardPadding)};
-
-    // Offset caused by title
-    // boardTopLeft.y += verticalTitleSize + 2 * titlePadding;
-
-    // rectangle(boardTopLeft.x, boardTopLeft.y, boardTopLeft.x + tileSize, boardTopLeft.y + tileSize);
 
     for (short rowNum { 1 }; rowNum <= numOfTilesOnSide; rowNum++) {
         for (short colNum { 1 }; colNum <= numOfTilesOnSide; colNum++) {
