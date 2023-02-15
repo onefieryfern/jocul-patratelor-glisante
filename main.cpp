8d343ad2a2a4374b953870ad6897b1be00fd6b46
Andrei Florea <122226601+onefieryfern@users.noreply.github.com>
2023-02-15 15:17:39 +0000

    Initialise window with user-inputted settings

--- main.cpp
+++ main.cpp
@@ -6,15 +6,29 @@ struct Point {
     short y{};
 };
 
-int main() {
-    // Window Properties
-    constexpr short xAxisLength { 1280 };
-    constexpr short yAxisLength { 720 };
+short readWithValidation (short minIncl, short maxIncl) {
+    short data{};
 
+    do {
+        std::cin >> data;
+    } while (data < minIncl || data > maxIncl);
+
+    return data;
+}
+
+int main() {
+    // Read data
+    std::cout << "Window Properties: ";
+
+    std::cout << "X Axis [0 - 1280]: ";
+    const short xAxisLength { readWithValidation(0, 1280) };
+    std::cout << "Y Axis [0 - 720]: ";
+    const short yAxisLength { readWithValidation(0 , 720) };
+
+    // Window Properties
     constexpr char title[] { "Jocul Patratelor Glisante" };
 
     // Title Properties
-
     const short verticalTitleSize { 100 };
     constexpr short titlePadding { 20 };
 
@@ -24,24 +38,25 @@ int main() {
     const short tileSize { 80 };
 
     const short boardSize { numOfTilesOnSide * tileSize };
-    const short boardPadding { (yAxisLength - (titlePadding + verticalTitleSize) - boardSize) / 2 };
+    const short boardPadding { static_cast<short>((yAxisLength - (titlePadding + verticalTitleSize) - boardSize) / 2) };
 
     // Other setup
-
     const short minBoardPadding { titlePadding };
-    const short maxBoardSize { yAxisLength - (titlePadding + verticalTitleSize + 2 * minBoardPadding) };
+    const short maxBoardSize { static_cast<short>(yAxisLength - (titlePadding + verticalTitleSize + 2 * minBoardPadding)) };
     const short maxTileSize { static_cast<short>(maxBoardSize / numOfTilesOnSide) };
 
-    const short minTileSize { yAxisLength <= xAxisLength ? yAxisLength / 9 : xAxisLength / 16 };
-    const short minBoardSize { numOfTilesOnSide * minTileSize };
-    const short maxBoardPadding { (yAxisLength - (titlePadding + verticalTitleSize) - minBoardSize) / 2 };
+    const short minTileSize { static_cast<short>(yAxisLength <= xAxisLength ? yAxisLength / 9 : xAxisLength / 16) };
+    const short minBoardSize { static_cast<short>(numOfTilesOnSide * minTileSize) };
+    const short maxBoardPadding { static_cast<short>((yAxisLength - (titlePadding + verticalTitleSize) - minBoardSize) / 2) };
 
+    //
     initwindow(xAxisLength, yAxisLength, title);
+    std::cout << "Window has been created!\n";
 
     // Draw title placeholder
     rectangle(titlePadding, titlePadding, xAxisLength - titlePadding, verticalTitleSize + titlePadding);
 
-    Point boardTopLeft { (xAxisLength - boardSize) / 2, (titlePadding + verticalTitleSize + boardPadding)};
+    Point boardTopLeft { static_cast<short>((xAxisLength - boardSize) / 2), static_cast<short>((titlePadding + verticalTitleSize + boardPadding)) };
 
     for (short rowNum { 1 }; rowNum <= numOfTilesOnSide; rowNum++) {
         for (short colNum { 1 }; colNum <= numOfTilesOnSide; colNum++) {
