dd32908edd8f872a66d851ee15f6085c20037692
Andrei Florea <122226601+onefieryfern@users.noreply.github.com>
2023-02-23 16:05:05 +0200

    Finish rough draft of game

--- main.cpp
+++ main.cpp
@@ -8,11 +8,33 @@ struct Point {
     int y{};
 };
 
+struct Position {
+    int row{};
+    int col{};
+};
+
 struct DrawableText {
     Point topLeft{};
 
     std::string text{};
     textsettingstype settings{};
+};
+
+struct TextBox {
+    Point topLeft{};
+
+    int verticalSize{};
+    int horizontalSize{};
+
+    int outsidePadding{};
+    int insidePadding{};
+
+    int verticalTextSpace{};
+    int horizontalTextSpace{};
+
+    fillsettingstype background{};
+
+    DrawableText drawableText{};
 };
 
 struct Window {
@@ -61,7 +83,7 @@ enum pieces {
 };
 
 struct GameStatus {
-    Point emptySpace{};
+    Position emptySpace{};
 };
 
 struct PlayerPreferences {
@@ -73,7 +95,11 @@ enum directions {
     left = 0, up = 1, right = 2, down = 3
 };
 
-Point movementInstructions[4] { {-1, 0}, {0, -1}, {+1, 0}, {0, +1} };
+constexpr int numOfDirections = 4;
+constexpr Position movementInstructions[numOfDirections] { {0, -1}, {-1, 0}, {0, +1}, {+1, 0} };
+
+constexpr int keyToDirection[][numOfDirections] { {KEY_LEFT, KEY_UP, KEY_RIGHT, KEY_DOWN}, {left, up, right, down} };
+constexpr directions oppositeDirections[numOfDirections] {right, down, left, up};
 
 TitleBox getTitleBox (int verticalSize, int outsidePadding, int insidePadding, const Window& window) {
     TitleBox titleBox{ .verticalSize = verticalSize, .outsidePadding = outsidePadding, .insidePadding = insidePadding, };
@@ -87,7 +113,7 @@ TitleBox getTitleBox (int verticalSize, int outsidePadding, int insidePadding, c
 Title getTitle (const TitleBox& titleBox, const std::string& text, const textsettingstype& textSettings) {
     Title title { .box = titleBox, .drawableText = { .text = text, .settings = textSettings } };
 
-    // Find maximum text sideSize
+    // Find maximum text size
     textsettingstype initialTextSettings{};
     gettextsettings(&initialTextSettings);
     settextstyle(title.drawableText.settings.font, title.drawableText.settings.direction, title.drawableText.settings.charsize);
@@ -100,6 +126,85 @@ Title getTitle (const TitleBox& titleBox, const std::string& text, const textset
     settextstyle(initialTextSettings.font, initialTextSettings.direction, initialTextSettings.charsize);
 
     return title;
+}
+
+int getMaxTextSize (const char* text, int maxVerticalSpace, textsettingstype textSettings) {
+    constexpr int maxCharSize { 10 };
+
+    textsettingstype initialTextSettings{};
+    gettextsettings(&initialTextSettings);
+
+    int maxTitleCharSize{ 1 };
+    for (int i = 1; i <= maxCharSize; i++) {
+        settextstyle(textSettings.font, textSettings.direction, i);
+
+        if (textheight(const_cast<char*>(text)) <= maxVerticalSpace) {
+            maxTitleCharSize = i;
+        }
+        else {
+            break;
+        }
+    }
+
+    settextstyle(initialTextSettings.font, initialTextSettings.direction, initialTextSettings.charsize);
+
+    return maxTitleCharSize;
+}
+
+TextBox getTextBox (int yCoordinate, int outerPadding, int verticalSize, int innerPadding, int xAxisLength, fillsettingstype background, std::string text, textsettingstype textSettings) {
+    TextBox textBox{};
+
+    textBox.outsidePadding = outerPadding;
+    textBox.insidePadding = innerPadding;
+
+    textBox.topLeft = {textBox.outsidePadding, yCoordinate };
+    textBox.verticalSize = verticalSize;
+    textBox.horizontalSize = xAxisLength - 2 * textBox.outsidePadding;
+
+    textBox.verticalTextSpace = textBox.verticalSize - 2 * textBox.insidePadding;
+    textBox.horizontalTextSpace = textBox.horizontalSize - 2 * textBox.insidePadding;
+
+    textBox.background = background;
+
+    textBox.drawableText.text = text;
+    textBox.drawableText.settings = { textSettings.font, textSettings.direction, getMaxTextSize(const_cast<char*>(textBox.drawableText.text.c_str()), textBox.verticalSize, textSettings) };
+
+    //
+    textsettingstype currentTextSettings{};
+    gettextsettings(&currentTextSettings);
+    settextstyle(textBox.drawableText.settings.font, textBox.drawableText.settings.direction, textBox.drawableText.settings.charsize);
+
+    textBox.drawableText.topLeft.x = textBox.topLeft.x + ( textBox.horizontalTextSpace - textwidth(const_cast<char*>(textBox.drawableText.text.c_str())) ) / 2;
+    textBox.drawableText.topLeft.y = textBox.topLeft.y + ( textBox.verticalTextSpace - textheight(const_cast<char*>(textBox.drawableText.text.c_str())) ) / 2;
+
+    settextstyle(currentTextSettings.font, currentTextSettings.direction, currentTextSettings.charsize);
+
+    return textBox;
+}
+
+void drawTextBox (const TextBox& textBox) {
+    // Store initial settings
+    fillsettingstype currentFillSettings{};
+    getfillsettings(&currentFillSettings);
+
+    textsettingstype currentTextSettings{};
+    gettextsettings(&currentTextSettings);
+
+    auto currentBkColour = static_cast<colors>(getbkcolor());
+
+    // Set TextBox settings
+    setfillstyle(textBox.background.pattern, textBox.background.color);
+    settextstyle(textBox.drawableText.settings.font, textBox.drawableText.settings.direction, textBox.drawableText.settings.charsize);
+    setbkcolor(textBox.background.color);
+
+    // Draw TextBox
+    bar(textBox.topLeft.x, textBox.topLeft.y, textBox.topLeft.x + textBox.horizontalSize, textBox.topLeft.y + textBox.verticalSize);
+    outtextxy(textBox.drawableText.topLeft.x, textBox.drawableText.topLeft.y, const_cast<char*>(textBox.drawableText.text.c_str()));
+
+    // Restore initial settings
+    setfillstyle(currentFillSettings.pattern, currentFillSettings.color);
+    settextstyle(currentTextSettings.font, currentTextSettings.direction, currentTextSettings.charsize);
+    setbkcolor(currentBkColour);
 }
 
 Board getBoard (int tilesOnSide, int tileSize, int tilePadding, int padding, const TitleBox& titleBox, const Window& window) {
@@ -157,55 +262,84 @@ void drawBoard (const Board& board, pieces matrix[][5], const PlayerPreferences&
     }
 }
 
+void eraseBoard (const Board& board) {
+    fillsettingstype currentFillSettings{};
+    getfillsettings(&currentFillSettings);
+
+    fillsettingstype erase {SOLID_FILL, getbkcolor()};
+    setfillstyle(erase.pattern, erase.color);
+    bar(board.topLeft.x, board.topLeft.y, board.topLeft.x + board.sideSize, board.topLeft.y + board.sideSize);
+
+    setfillstyle(currentFillSettings.pattern, currentFillSettings.color);
+}
+
 int getMaxTitleSize (const char* text, TitleBox titleBox, textsettingstype textSettings) {
-    constexpr int maxCharSize { 10 };
+    return getMaxTextSize(text, titleBox.verticalTextSpace, textSettings);
+}
 
-    textsettingstype initialTextSettings{};
-    gettextsettings(&initialTextSettings);
+bool isInBounds (const Board& board, const Position& position) {
+    if (position.row < 0 || position.row >= board.tilesOnSide) {
+        return false;
+    }
+    if (position.col < 0 || position.col >= board.tilesOnSide) {
+        return false;
+    }
 
-    int maxTitleCharSize{ 1 };
-    for (int i = 1; i <= maxCharSize; i++) {
-        settextstyle(textSettings.font, textSettings.direction, i);
+    return true;
+}
 
-        if (textheight(const_cast<char*>(text)) <= (titleBox.verticalSize - 2 * titleBox.insidePadding)) {
-            maxTitleCharSize = i;
-        }
-        else {
+bool isOccupied (pieces matrix[][5], const Position& position) {
+    return matrix[position.row][position.col] != empty;
+}
+
+bool canMoveInDirection (const Board& board, const GameStatus& gamestatus, pieces matrix[][5], directions direction, pieces currentPlayer) {
+    Position from { gamestatus.emptySpace.row + movementInstructions[oppositeDirections[direction]].row, gamestatus.emptySpace.col + movementInstructions[oppositeDirections[direction]].col };
+
+    if (matrix[from.row][from.col] != currentPlayer) {
+        return false;
+    }
+    if (!isInBounds(board, from)) {
+        return false;
+    }
+    if (isOccupied(matrix, gamestatus.emptySpace)) {
+        return false;
+    }
+
+    return true;
+}
+
+void moveInDirection (GameStatus& gamestatus, pieces matrix[][5], directions direction) {
+    Position from { gamestatus.emptySpace.row + movementInstructions[oppositeDirections[direction]].row, gamestatus.emptySpace.col + movementInstructions[oppositeDirections[direction]].col };
+
+    matrix[gamestatus.emptySpace.row][gamestatus.emptySpace.col] = matrix[from.row][from.col];
+    matrix[from.row][from.col] = empty;
+
+    gamestatus.emptySpace = from;
+}
+
+pieces whoHasWon (const Board& board, pieces matrix[][5], const GameStatus& gameStatus) {
+    pieces firstPieceChecked{};
+    for (int i = 0; i < numOfDirections; i++) {
+        Position positionToBeChecked { gameStatus.emptySpace.row + movementInstructions[i].row, gameStatus.emptySpace.col + movementInstructions[i].col };
+
+        if (isInBounds(board, positionToBeChecked)) {
+            firstPieceChecked = matrix[positionToBeChecked.row][positionToBeChecked.col];
             break;
         }
     }
 
-    settextstyle(initialTextSettings.font, initialTextSettings.direction, initialTextSettings.charsize);
+    for (int i = 0; i < numOfDirections; i++) {
+        Position positionToBeChecked { gameStatus.emptySpace.row + movementInstructions[i].row, gameStatus.emptySpace.col + movementInstructions[i].col };
 
-    return maxTitleCharSize;
-}
-
-bool isInBounds (const Board& board, const Point& position) {
-    if (position.x < 0 || position.x >= board.tilesOnSide) {
-        return false;
-    }
-    if (position.y < 0 || position.y >= board.tilesOnSide) {
-        return false;
+        if (!isInBounds(board, positionToBeChecked)) {
+            continue;
+        }
+        if (matrix[positionToBeChecked.row][positionToBeChecked.col] != firstPieceChecked) {
+            return empty; // has not won
+        }
     }
 
-    return true;
-}
-
-bool isOccupied (pieces matrix[][5], const Point& position) {
-    return matrix[position.x][position.y] != empty;
-}
-
-bool canMoveInDirection (const Board& board, pieces matrix[][5], const Point& from, const directions& direction) {
-    Point to { from.x + movementInstructions[direction].x, from.y + movementInstructions[direction].y };
-
-    if (!isInBounds(board, to)) {
-        return false;
-    }
-    if (isOccupied(matrix, to)) {
-        return false;
-    }
-
-    return true;
+    return firstPieceChecked; // has won
 }
 
 int main() {
@@ -248,10 +382,10 @@ int main() {
 
     // Create board matrix
     pieces boardMatrix[numOfTilesOnSide][numOfTilesOnSide];
-    pieces nextPiece { playerOne };
+    pieces nextPiece { playerTwo };
     for (int i = 0; i < numOfTilesOnSide; i++) {
         for (int j = 0; j < numOfTilesOnSide; j++) {
-            if (i == gameStatus.emptySpace.x && j == gameStatus.emptySpace.y) {
+            if (i == gameStatus.emptySpace.row && j == gameStatus.emptySpace.col) {
                 boardMatrix[i][j] = empty;
             }
             else {
@@ -263,6 +397,70 @@ int main() {
 
     // Draw board
     drawBoard(largestBoard, boardMatrix, playerPreferences);
+
+    bool firstMoveMade { false };
+    pieces playerTurn { playerOne };
+    while (whoHasWon(largestBoard, boardMatrix, gameStatus) == empty || !firstMoveMade) {
+        directions movementDirection {};
+        bool moveMade { false };
+
+        if (playerTurn == playerOne) {
+            bool gotValidInput{ false };
+
+            if (kbhit()) {
+                int input{getch()};
+                for (int i = 0; i < numOfDirections; i++) {
+                    if (input == keyToDirection[0][i]) {
+                        movementDirection = static_cast<directions>(keyToDirection[1][i]);
+                        gotValidInput = true;
+                    }
+                }
+            }
+
+            if (gotValidInput && canMoveInDirection(largestBoard, gameStatus, boardMatrix, movementDirection, playerTurn)) {
+                moveInDirection(gameStatus, boardMatrix, movementDirection);
+
+                eraseBoard(largestBoard);
+                drawBoard(largestBoard, boardMatrix, playerPreferences);
+
+                if (!firstMoveMade) {
+                    firstMoveMade = true;
+                }
+
+                moveMade = true;
+            }
+        }
+        else { // playerTurn == playerTwo
+            while (!moveMade) {
+                auto randomDirection = static_cast<directions>(std::rand() % 4);
+
+                if (canMoveInDirection(largestBoard, gameStatus, boardMatrix, randomDirection, playerTurn)) {
+                    moveInDirection(gameStatus, boardMatrix, randomDirection);
+
+                    eraseBoard(largestBoard);
+                    drawBoard(largestBoard, boardMatrix, playerPreferences);
+
+                    moveMade = true;
+                }
+            }
+        }
+
+        if (moveMade) {
+            playerTurn = playerTurn == playerOne ? playerTwo : playerOne;
+        }
+
+        delay(100);
+    }
+
+    TextBox playerOneWin { getTextBox(20, 40, 160, 5, window.xAxisLength, {SOLID_FILL, playerPreferences.player1.color}, "Player One has won!"s, { .font = SANS_SERIF_FONT, .direction = HORIZ_DIR }) };
+    TextBox playerTwoWin { getTextBox(20, 40, 160, 5, window.xAxisLength, {SOLID_FILL, playerPreferences.player2.color}, "Player Two has won!"s, { .font = SANS_SERIF_FONT, .direction = HORIZ_DIR }) };
+
+    if (whoHasWon(largestBoard, boardMatrix, gameStatus) == playerOne) {
+        drawTextBox(playerOneWin);
+    }
+    else {
+        drawTextBox(playerTwoWin);
+    }
 
     // Close window
     getch();
